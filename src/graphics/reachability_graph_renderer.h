5bfb3865ee220ffb17bdd69a2e24e003bb0c33e4
Krona-org <danil.marushkin@yandex.ru>
2025-12-17 23:33:50 +0300

    пока просто

--- src/graphics/reachability_graph_renderer.h
+++ src/graphics/reachability_graph_renderer.h
@@ -11,6 +11,15 @@
 class ReachabilityGraphRenderer
 {
 public:
+
+    struct Label3D
+    {
+        glm::vec3 worldPos;
+        std::string text;
+    };
+    const std::vector<Label3D>& GetNodeLabels() const { return m_NodeLabels; }
+    const std::vector<Label3D>& GetEdgeLabels() const { return m_EdgeLabels; }
+
     explicit ReachabilityGraphRenderer(Scene& scene);
     ~ReachabilityGraphRenderer();
 
@@ -28,4 +37,7 @@ private:
 
     std::vector<Sphere*> m_NodeSpheres;
     std::vector<Line*>   m_EdgeLines;
+    std::vector<Label3D> m_NodeLabels;
+    std::vector<Label3D> m_EdgeLabels;
+
 };
